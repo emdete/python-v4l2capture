7c6e7322e952e540c3fa8e3e03eede8f0a90d386
M. Dietrich <mdt@emdete.de>
2019-07-11 16:04:50 +0200

    adapt to new features

--- list_devices.py
+++ list_devices.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # python-v4l2capture
 #
@@ -15,14 +15,20 @@ import v4l2capture
 file_names = [x for x in os.listdir("/dev") if x.startswith("video")]
 file_names.sort()
 for file_name in file_names:
-    path = "/dev/" + file_name
-    print path
-    try:
-        video = v4l2capture.Video_device(path)
-        driver, card, bus_info, capabilities = video.get_info()
-        print "    driver:       %s\n    card:         %s" \
-            "\n    bus info:     %s\n    capabilities: %s" % (
-                driver, card, bus_info, ", ".join(capabilities))
-        video.close()
-    except IOError, e:
-        print "    " + str(e)
+	path = "/dev/" + file_name
+	print(path)
+	try:
+		video = v4l2capture.Video_device(path)
+		driver, card, bus_info, capabilities = video.get_info()
+		print("    driver:       {}\n    card:         {}" \
+			"\n    bus info:     {}\n    capabilities: {}".format(
+				driver, card, bus_info, ", ".join([n.decode() for n in capabilities])))
+		print('format', video.get_format())
+		for fourcc in video.get_formats():
+			for framesize in video.get_framesizes(fourcc):
+				x, y = framesize['size_x'], framesize['size_y']
+				for frameinterval in video.get_frameintervals(fourcc, x, y):
+					print('\t', fourcc.decode(), x, y, frameinterval['fps'])
+		video.close()
+	except IOError as e:
+		print("    " + str(e))
